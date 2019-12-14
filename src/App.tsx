6978180cc145b63f69c58100eb9cb81be3acd97a
au.zhurov <alexzhurovbiz@gmail.com>
2019-12-14 18:53:55 +0200

    added semantic ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,39 @@
-import React       from 'react';
-import { connect } from 'react-redux';
-import {
-  IAppProps,
-  mapDispatchToProps,
-  mapStateToProps
-}                  from './store';
+import React                                              from 'react';
+import { connect }                                        from 'react-redux';
+import { IAppProps, mapDispatchToProps, mapStateToProps } from './store';
 import './styles/App.css';
-import Converter   from './components/Converter';
-import Currencies  from './components/Currencies';
-import { Header }  from './components/Header';
-
-const TAB_CONVERTER = 'Converter';
-const TAB_CURRENCIES = 'Currencies';
+import Converter                                          from './components/Converter';
+import Currencies                                         from './components/Currencies';
+import { Container, Tab }                                 from 'semantic-ui-react';
 
 class App extends React.Component<IAppProps> {
 
-  state = {
-    tabs: [
-      { id: TAB_CONVERTER, title: 'Converter', isActive: false },
-      { id: TAB_CURRENCIES, title: 'Currencies', isActive: false }
-    ]
-  };
-
-  async initData(): Promise<void> {
+  async componentDidMount(): Promise<void> {
     await this.props.thunkGetIpInfo();
     await this.props.thunkGetCurrencies();
-    // this.props.thunkGetRates(this.props.ipInfo.currency);
-  }
-
-  async componentDidMount(): Promise<void> {
-    await this.initData();
-    this.setActiveTab(TAB_CONVERTER);
-    // this.setActiveTab(TAB_CURRENCIES);
+    this.props.thunkGetRates(this.props.ipInfo.currency);
   };
 
-  /**
-   * Tab click handler
-   * @param {string} tabName
-   */
-  protected setActiveTab(tabName: string): void {
-    const tabs = this.state.tabs.map(tab => ({
-      ...tab,
-      isActive: tab.id === tabName
-    }));
-    this.setState({ tabs });
-  }
-
-
   render() {
-    const { tabs } = this.state;
-    const { currencies } = this.props;
-    const activeTab = tabs.find(({ isActive }) => isActive) || tabs[0];
+    const panes = [{
+      menuItem: 'Converter',
+      render: () => (<Tab.Pane><Converter/></Tab.Pane>)
+    }, {
+      menuItem: 'Currencies',
+      render: () => (<Tab.Pane><Currencies/></Tab.Pane>)
+    }];
     return (
       <div className="App">
-        <Header tabs={tabs} currencies={currencies} setActiveTab={this.setActiveTab.bind(this)}/>
-
-        <main className="App__main">
-          {(() => {
-            switch (activeTab.id) {
-              case TAB_CONVERTER:
-                return <Converter/>;
-              case TAB_CURRENCIES:
-                return <Currencies/>;
-              default:
-                return <div>404 page</div>;
-            }
-          })()}
-        </main>
+        <Container>
+          <Tab
+            menu={{
+              color: 'grey',
+              tabular: true,
+              attached: true,
+              inverted: true
+            }}
+            panes={panes}/>
+        </Container>
       </div>
     );
   }
